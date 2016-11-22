7981c91f1823a45a98f5167a9684fb89bae6b3af
Joseph Muruguru <jmuru1@bu.edu>
2016-11-21 21:51:45 -0500

    camera-activation-added

--- photo-text-analyzer/ViewController.swift
+++ photo-text-analyzer/ViewController.swift
@@ -8,18 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate
+{
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+
+    @IBOutlet weak var imageContainer: UIImageView!
+
+    @IBAction func activateCmera(_ sender: Any) {
+        //check if camera is available on device
+        if (UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera)) {
+            let imagePicker = UIImagePickerController();
+            imagePicker.delegate = self;
+            imagePicker.sourceType = UIImagePickerControllerSourceType.camera;
+            imagePicker.allowsEditing = false;
+            self.present(imagePicker, animated: true, completion: nil)
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 
 }
 
